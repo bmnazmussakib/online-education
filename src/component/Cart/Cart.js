26c5ce069a95dcb831ddd174a6f5ccb26b9f6e2d
bmnazmussakib <nazmussakib161@gmail.com>
2020-10-11 12:08:21 +0600

    updated for deploy

--- src/component/Cart/Cart.js
+++ src/component/Cart/Cart.js
@@ -14,8 +14,8 @@ const Cart = (props) => {
     return (
         <div className='cart'>
             <h1>Order Summery</h1>
-            <h4>Item added : {item.length}</h4>
-            <h4>Price : {totalPrice}</h4>
+            <h4>Course added : {item.length}</h4>
+            <h4>Total price : $ {totalPrice}</h4>
             <button className="enroll-btn">Place Order</button>
         </div>
     );
--- src/component/Shop/Shop.js
+++ src/component/Shop/Shop.js
@@ -8,37 +8,37 @@ const Shop = () => {
     const [course, setCourse] = useState([]);
     const [cart, setCart] = useState([]);
 
-    useEffect(()=>{
-        fetch('http://www.json-generator.com/api/json/get/cgusNUfIwO?indent=3')
-        .then(res => res.json())
-        .then(data => setCourse(data));
-        
-    },[])
+    useEffect(() => {
+        fetch('https://www.json-generator.com/api/json/get/cgusNUfIwO?indent=3')
+            .then(res => res.json())
+            .then(data => setCourse(data));
+
+    }, [])
 
     let addHandler = (course) => {
-        
-        
+
+
         //console.log("course added" , course);
         let newCart = [...cart, course];
         setCart(newCart);
         //console.log(newCart);
-        
+
     }
 
-    
-    
-    
+
+
+
     return (
         <div className='shop-container'>
             <div className="course-container">
-                
+
                 {
                     course.map(coursess => <Course addHandler={addHandler} coursess={coursess} ></Course>)
                 }
-                
+
 
             </div>
-            
+
             <div className="cart-container">
                 <Cart cart={cart}></Cart>
             </div>
